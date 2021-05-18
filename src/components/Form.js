b786cc8407ff99441b9c3f95f9672d1c7cab0420
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-18 14:39:41 -0700

    Complete form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,7 +16,7 @@ export default function Form(props) {
     return (
         <form className='form container' onSubmit={onSubmit}>
             <div className='form-group inputs'>
-                <label>First name
+                <label>First Name:
                     <input 
                     type='text'
                     name='firstName'
@@ -24,6 +24,36 @@ export default function Form(props) {
                     value={values.firstName}
                     />
                 </label>
+                <label>Last Name:
+                    <input 
+                    type='text'
+                    name='lastName'
+                    onChange={onChange}
+                    value={values.lastName}
+                    />
+                </label>
+                <label>Email:
+                    <input 
+                    type='text'
+                    name='email'
+                    onChange={onChange}
+                    value={values.email}
+                    />
+                </label>
+                <label>Role:
+                    <select value={values.role} name='role' onChange={onChange}>
+                        <option value=''>-- Select a Role --</option>
+                        <option value='Front-End'>Front-End</option>
+                        <option value='Back-End'>Back-End</option>
+                        <option value='Data Science'>Data Science</option>
+                        <option value='UI Design'>UI Design</option>
+                        <option value='UX Design'>UX Design</option>
+                    </select>
+                </label>
+
+                <div className='submit'>
+                    <button disabled={!values.firstName || !values.lastName || !values.email || !values.role}>Submit</button>
+                </div>
             </div>
         </form>
     )
