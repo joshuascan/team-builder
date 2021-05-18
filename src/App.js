3ebae2c15ce17767d137ee3bf55ebc0ec646bf4e
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-18 14:20:44 -0700

    Build app page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import TeamMember from './components/TeamMember';
+
+const initialFormValues = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    role: '',
+}
 
 function App() {
+
+    const [teamMembers, setTeamMembers] = useState([])
+    const [formValues, setFormValues] = useState(initialFormValues)
+
+    const updateForm = (inputName, inputValue) => {
+        setFormValues({ ...formValues, [inputName]: inputValue })
+    }
+
+    const submitForm = () => {
+        const newTeamMember = {
+            firstName: formValues.firstName.trim(),
+            lastName: formValues.lastName.trim(),
+            email: formValues.email.trim(),
+            role: formValues.role,
+        }
+        setTeamMembers([...teamMembers, newTeamMember])
+        setFormValues(initialFormValues)
+    }
+
   return (
     <div className="App">
-
+        <h1>Team Member Tracker</h1>
+        <h2>Add Team Member:</h2>
+        {/* <Form
+        update={updateForm}
+        submit={submitForm}
+        values={formValues}
+        /> */}
+        <h2>Team Members:</h2>
+        {/* {
+            teamMembers.map(member, idx => {
+                return (
+                    <TeamMember key={idx} details={member} />
+                )
+            })
+        } */}
     </div>
   );
 }
